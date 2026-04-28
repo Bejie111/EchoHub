c7e349cb69d28985a9bc7e482b21800647933b11
Roldan Nino Cayao <roldancayao17@gmail.com>
2026-04-28 23:02:57 +0800

    naa koy gi usab sa kaning HomeController sa return niya kay ako gipa redirect to Login na view sa Account

--- EchoHub/Controllers/HomeController.cs
+++ EchoHub/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace EchoHub.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return RedirectToAction("Login", "Account");
         }
 
         public IActionResult Privacy()
