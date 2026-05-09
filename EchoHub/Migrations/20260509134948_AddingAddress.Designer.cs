0d1faeecef384f0be46f7341395cb8a3f68a2a4e
Roldan Nino Cayao <roldancayao17@gmail.com>
2026-05-09 23:21:10 +0800

    Nag add nasad kog Address diay pero sa migration rani ambot ngano di nako ma pass ang model nag change man ta ko didto mao ni request ni pogpog ANG ADDRESSS

--- EchoHub/Migrations/20260509134948_AddingAddress.Designer.cs
+++ EchoHub/Migrations/20260509134948_AddingAddress.Designer.cs
@@ -4,6 +4,7 @@ using EchoHub.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EchoHub.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260509134948_AddingAddress")]
+    partial class AddingAddress
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -133,10 +136,6 @@ namespace EchoHub.Migrations
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Password")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("PhoneNumber")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
--- EchoHub/Migrations/20260509144024_AddingPhoneNumber.Designer.cs
+++ EchoHub/Migrations/20260509144024_AddingPhoneNumber.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EchoHub.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260509120819_DropStaffId")]
-    partial class DropStaffId
+    [Migration("20260509144024_AddingPhoneNumber")]
+    partial class AddingPhoneNumber
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -80,6 +80,9 @@ namespace EchoHub.Migrations
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("EwasteId"));
 
+                    b.Property<string>("Address")
+                        .HasColumnType("nvarchar(max)");
+
                     b.Property<string>("Category")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
@@ -133,6 +136,10 @@ namespace EchoHub.Migrations
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Password")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("PhoneNumber")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
