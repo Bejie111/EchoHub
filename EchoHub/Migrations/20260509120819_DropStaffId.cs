5445f2dc6bef4463c2d703315a613cad5aa35179
Roldan Nino Cayao <roldancayao17@gmail.com>
2026-05-09 20:13:49 +0800

    Nag himo rakog migration para ma drop ang StaffId, kung naa pa gihapon sa inyong database sa collections table

---
+++ EchoHub/Migrations/20260509120819_DropStaffId.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EchoHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class DropStaffId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+            name: "FK_Collections_Users_StaffId",
+            table: "Collections");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Collections_StaffId",
+                table: "Collections");
+
+            migrationBuilder.DropColumn(
+                name: "StaffId",
+                table: "Collections");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+            name: "StaffId",
+            table: "Collections",
+            type: "int",
+            nullable: false,
+            defaultValue: 0);
+
+        migrationBuilder.CreateIndex(
+            name: "IX_Collections_StaffId",
+            table: "Collections",
+            column: "StaffId");
+
+        migrationBuilder.AddForeignKey(
+            name: "FK_Collections_Users_StaffId",
+            table: "Collections",
+            column: "StaffId",
+            principalTable: "Users",
+            principalColumn: "Id",
+            onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
