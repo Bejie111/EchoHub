8aefc5e296c7abde12aec161cbd346cc8ed221ae
Roldan Nino Cayao <roldancayao17@gmail.com>
2026-05-09 21:39:41 +0800

    Gi change nako sa may user gi wala ang katong naka bullet nga warning, tanggal sad kog way mga gamit

--- EchoHub/Models/EwasteItem.cs
+++ EchoHub/Models/EwasteItem.cs
@@ -18,6 +18,8 @@ namespace EchoHub.Models
 
         public string Status { get; set; }
 
+        public string? Address { get; set; }
+
         public DateTime DateSubmitted { get; set; }
 
         public int UserId { get; set; }
